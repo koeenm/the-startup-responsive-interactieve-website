ac9411d0e876199ad13645b0d7cb15a1fff38e1d
koeenm <mkoeen2@gmail.com>
2024-01-22 11:43:44 +0100

    livestream start proberen

--- script.js
+++ script.js
@@ -9,3 +9,34 @@ hamburger.addEventListener("click", function() {
     navMenu.classList.toggle("active");
     console.log('4')
 });
+
+    // audio player
+    // 1. Selecteren van element via class
+    const playradio = document.querySelector('.play-radio');
+    const audio = document.querySelector('audio');
+    const count = 0
+
+    // 2. Event
+    function playPause(){
+      if(count == 0){
+        count = 1;
+        audio.play();
+      }else{
+        count = 0;
+        audio.pause();
+      }
+    }
+
+    // text
+    // 1. Selecteren van element via class
+    const changeTextplay = document.querySelector('.play-radio-text');
+    const changeTextpause = document.querySelector('.pause-radio-text');
+    const playSvg = document.querySelector('.play-radio-svg');
+    const pauseSvg = document.querySelector('.pause-radio-svg');
+
+    // 2. Event
+    playradio.addEventListener('click', function (){
+      changeTextplay.classList.toggle('hide')
+      playSvg.classList.toggle('hide')
+    })
+
